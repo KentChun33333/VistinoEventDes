427b401e849c04bbe2131ca264db30a54ae23633
KentChun33333 <kentchun33333@foxmail.com>
2016-10-22 09:17:52 -0400

    -,

---
+++ Mul_Obj_Trackery/vatic_data_parser.py
@@ -0,0 +1,108 @@
+#==============================================================================
+# Author : Kent Chiu (kentchun33333@gmail.com)
+#==============================================================================
+# Notice
+# - This recipe is used to parser the labeled data from vatic
+# -----------------------------------------------------------------------------
+# Mode Selection
+# - Mode A = Extraction with positive image
+# - Mode B = Extraction with negative image
+# -----------------------------------------------------------------------------
+# Data Description
+# 'track_id' : ' All rows with the same ID belong to the same path.',
+# 'xmin'     : ' The top left x-coordinate of the bounding box.',
+# 'ymin'     : ' The top left y-coordinate of the bounding box.',
+# 'xmax'     : ' The bottom right x-coordinate of the bounding box.',
+# 'ymax'     : ' The bottom right y-coordinate of the bounding box.',
+# 'frame'    : ' The frame that this annotation represents.',
+# 'lost'     : ' If 1, the annotation is outside of the view screen.',
+# 'occluded' : ' If 1, the annotation is occluded.',
+# 'generated': ' If 1, the annotation was automatically interpolated.',
+# 'label'    : ' label for this annotation, enclosed in quotation marks.    '
+# -----------------------------------------------------------------------------
+# Usage Example
+# -----------------------------------------------------------------------------
+#
+#
+#==============================================================================
+
+import argparse
+import cv2
+import imageio
+import pandas as pd
+from skimage.io import imsave, imshow
+import os
+
+#==============================================================================
+# Get the command-line variables
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f','--file', help='the labeled file path')
+parser.add_argument('-v','--vidPath', help='corresponding video path')
+parser.add_argument('-o','--output', help='output folder')
+parser.add_argument('-m','--mode', help='mode handler', type=str, nargs='+')
+arg = var(parser.parse_args())
+
+#==============================================================================
+# make sure output folder is not existed
+
+try:
+    assert os.path.isdir(arg['output'])==False
+except:
+    print ('the output folder is existed')
+os.makedirs(arg['output'])
+
+
+#==============================================================================
+# Get the video & labeled data
+
+vid = imageio.get_reader(arg['vidPath'])
+
+with open(arg['file'],'r') as f :
+    data = f.readlines()
+
+data2 = [i.split(' ') for i in data]
+
+
+col = ['track_id','xmin','ymin','xmax','ymax',
+       'frameid','lost','occluded','generated','label_name']
+
+df = pd.DataFrame(data2,columns=col )
+
+#==============================================================================
+# Hard Mining
+
+def gen_hard_mining(vid, df)
+    for frameID in range(vid.max_length):
+        frame = vid.get_data(frameID)
+        # get the meta data
+        df_tmp = df[ df['frameid']==str(frameID) ]
+        df_tmp = df_tmp[['xmin','ymin','xmax','ymax']]
+        for i_list in df_tmp.values:
+            startX, startY, endX, endY = i_list[0]
+            cv2.rectangle(frame, (startX,startY), (endX,endY), (0,0,0), -1)
+        yield frame
+
+#==============================================================================
+# Hard Mining
+
+def positive_img(vid, df, label):
+    for frameID in range(vid.max_length):
+        frame = vid.get_data(frameID)
+        # get the meta data
+        df_tmp = df[ df['frameid']==str(frameID) ]
+        df_tmp = df_tmp[ df_tmp['label_name']==label ]
+        df_tmp = df_tmp[['xmin','ymin','xmax','ymax']]
+        for i_list in df_tmp.values:
+            startX, startY, endX, endY = i_list[0]
+            img = frame[startX:endX, startY:endY,:]
+        yield img
+
+if __name__ == '__main__':
+    for _, img in enumerate(gen_hard_mining(vid, df)):
+        imsave(arg['output']+'\\BG\\'+str(_)+'.png')
+    labels = list(set(df['label_name']))
+    for label in labels:
+        for _, img in enumerate(positive_img(vid, df, label)):
+            label_folder = label.replace('"',"").split('\n')[0]
+            imsave(arg['output']+'\\{}\\'.format(label_folder)+str(_)+'.png')
